71fc182cd9f5fa08101eac0da8f4a2be2f88c4ab
cheyenne <cheyenne.furlong@my.jcu.edu.au>
2022-10-19 21:34:36 +1000

    Add word_occurrences program.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,8 +1,8 @@
-COLOUR_to_CODE = {"AliceBlue:""#f0f8ff", "Alizarin crimson:""#e32636","Amethyst:""#9966cc","Baby pink:""#f4c2c2",
-               "Banana Mania:""#fae7b5"}
+"""Hex colours program"""
+COLOUR_to_CODE = {"AliceBlue:""#f0f8ff", "Alizarin crimson:""#e32636", "Amethyst:""#9966cc", "Baby pink:""#f4c2c2",
+                  "Banana Mania:""#fae7b5"}
 # remember to put colour before I finish
 colour = input("Colour: ")
 while colour != "":
     print(f"the code for \"{colour}\" is {COLOUR_to_CODE.get(colour)}")
     colour = input("Colour: ")
-
