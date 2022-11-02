0fa1030bf1ee259ba91aa347483e2d4987995bea
cheyenne <cheyenne.furlong@my.jcu.edu.au>
2022-11-02 23:30:09 +1000

    Added testing code for client to use.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,7 +1,6 @@
 """Hex colours program"""
 COLOUR_to_CODE = {"AliceBlue:""#f0f8ff", "Alizarin crimson:""#e32636", "Amethyst:""#9966cc", "Baby pink:""#f4c2c2",
-                  "Banana Mania:""#fae7b5"}
-# remember to put colour before I finish
+                  "Banana Mania:""#fae7b5", "Barn Red:" "#7c0a02"}
 colour = input("Colour: ")
 while colour != "":
     print(f"the code for \"{colour}\" is {COLOUR_to_CODE.get(colour)}")
