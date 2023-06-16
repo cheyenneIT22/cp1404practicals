0d4824c998aab879bff39138441478ce7007d5d1
cheyenne <cheyenne.furlong@my.jcu.edu.au>
2023-06-16 10:30:24 +1000

    determines the score after user enters a number

---
+++ prac_02/score.py
@@ -0,0 +1,23 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+
+def main():
+    score = float(input("Enter score: "))
+    print(determine_status(score))
+
+
+def determine_status(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score > 50:
+        print("Passable")
+    elif score > 90:
+        print("Excellent")
+    else:
+        print("bad")
+
+
+main()
