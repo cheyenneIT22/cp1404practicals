80969573d79356bd6f3b3b55ab593f41592de736
cheyenne <cheyenne.furlong@my.jcu.edu.au>
2023-06-18 22:36:49 +1000

    Added files to main program to open and close file

---
+++ prac_03/files.py
@@ -0,0 +1,25 @@
+from prac_03.capitalist_conrad import out_file
+
+name = input("what is your name?: ")
+print(name, file=out_file)
+out_file.close()
+
+filename = "numbers.txt"
+in_file = open("numbers.txt", 'r')
+name = in_file.read().strip()
+in_file.close()
+print("your name is", name)
+
+in_file = open("numbers.txt", 'r')
+number1 = int(in_file.readline())
+number2 = int(in_file.readline())
+in_file.close()
+print(number1 + number2)
+
+in_file = open("numbers.txt", 'r')
+total = 0
+for line in in_file:
+    number = int(line)
+    total += number
+in_file.close()
+print(total)
