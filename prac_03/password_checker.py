b3daba73ed22450c50a2ad2346eacf8a96bc088c
cheyenne <cheyenne.furlong@my.jcu.edu.au>
2023-06-18 22:46:56 +1000

    Password checker menu program

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -37,7 +37,6 @@ def is_valid_password(password):
     count_digit = 0
     count_special = 0
     for char in password:
-        # TODO: count each kind of character (use str methods like isdigit)
         if char.isdigit():
             count_digit += 1
         elif char.islower():
@@ -47,11 +46,9 @@ def is_valid_password(password):
         elif char in SPECIAL_CHARACTERS:
             count_special += 1
 
-    # TODO: if any of the 'normal' counts are zero, return False
     if count_lower == 0 or count_upper == 0 or count_digit == 0:
         return False
 
-    # TODO: if special characters are required, then check the count of those
     # and return False if it's zero
     if SPECIAL_CHARS_REQUIRED:
         if count_special == 0:
