b239fbb0d3d35938063c40187cd6b0bf9cb80c43
cheyenne <cheyenne.furlong@my.jcu.edu.au>
2023-06-25 21:59:16 +1000

    Added Quick Pick Lottery Ticket Generator program

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,23 @@
+""""Quick Pick Lottery Ticket Generator"""
+import random
+
+NUMBER_OF_LINES = 6
+MINIMUM = 1
+MAXIMUM = 45
+
+
+def main():
+    number_of_picks = int(input("How many quick picks?: "))
+    while number_of_picks < 0:
+        print("That number is incorrect")
+        number_of_picks = int(input("How many quick picks?: "))
+
+    for i in range(number_of_picks):
+        quick_pick = []
+        for j in range(NUMBER_OF_LINES):
+            number = random.randint(MINIMUM, MAXIMUM)
+            while number in quick_pick:
+                number = random.randint(MINIMUM, MAXIMUM)
+            quick_pick.append(number)
+            quick_pick.sort()
+            print("".join(f"{number:2}" for number in quick_pick))
