d2a5272b8d6fd6c6656873b72ff52d081d782d48
cheyenne <cheyenne.furlong@my.jcu.edu.au>
2023-07-16 18:20:52 +1000

    Edited client code to import the cars classes

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -7,7 +7,7 @@ from prac_06 import cars
 
 def main():
     """Demo test code to show how to use car class."""
-    limo = car(180)
+    limo = cars(180)
     limo.drive(20)
     print(f"Car has fuel: {limo.fuel}")
     print(limo)
