2308fbe92626f1910e80c3024a967a30437011c1
cheyenne <cheyenne.furlong@my.jcu.edu.au>
2023-07-23 20:18:42 +1000

    edited the guitar program to include parts

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -7,12 +7,12 @@ guitars = []
 for line in file_in:
     line = line.strip()
     parts = line.split(",")
-    # print(parts)
-    # print(part for part in parts)
-    # guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
-    # name = parts[0]
-    # year = int(parts[1])
-    # cost = float(parts[2])
-    # guitars.append(Guitar(name, year, cost))
+    print(parts)
+    print(part for part in parts)
+    guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
+    name = parts[0]
+    year = int(parts[1])
+    cost = float(parts[2])
+    guitars.append(Guitar(name, year, cost))
     guitars.append(Guitar(parts[0], int(parts[1]), float(parts[2])))
 [print(guitar) for guitar in guitars]
