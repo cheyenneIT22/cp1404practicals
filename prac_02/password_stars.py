8c474d59b9e3018854b81b1a7e198af6cd91414f
cheye <cheyenne.furlong@my.jcu.edu.au>
2024-09-29 17:51:46 +1000

    Refactor password check program to use functions.

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,33 +1,25 @@
 """Secret Password program."""
-MINIMUM_LENGTH = 5
-
-
-def first_name():
-    """displays password user enters"""
-    password_name = input(f"Enter password of at least {MINIMUM_LENGTH} characters: ")
-    while len(password_name) < MINIMUM_LENGTH:
-        password_name = input(f"Enter password of at least {MINIMUM_LENGTH} characters: ")
-        print('*' * len(password_name))
+MIN_LENGTH = 5
 
 
 def main():
     """main function of program and displays the password"""
-    password = get_password(MINIMUM_LENGTH)
+    password = get_password({MIN_LENGTH})
     print_asterisks(password)
-
-
-def get_password(MINIMUM_LENGTH):
-    """determines the length of password based of number of characters"""
-    password = input(f"Enter password of at least {MINIMUM_LENGTH} characters: ")
-    while len(password) < MINIMUM_LENGTH:
-        print("password is too short.")
-        password = input(f"Enter password of at least {MINIMUM_LENGTH} characters: ")
-    return password
 
 
 def print_asterisks(sequence):
     """prints the password as asterisks sequence"""
     print('*' * len(sequence))
+
+
+def get_password(MIN_LENGTH):
+    """determines the length of password based of number of characters"""
+    password = input("Password: ")
+    while len(password) < MIN_LENGTH:
+        print("password is too short.")
+        password = input(f"Enter password of at least {MIN_LENGTH} characters: ")
+    return password
 
 
 main()
